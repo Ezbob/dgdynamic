600a6b18fc26c6ab46f18e36ff8996390b03b859
Ezbob <andersbusch@gmail.com>
2016-09-16 13:59:18 +0200

    initial commit of plugin module

---
+++ plugins/ode_matlab.py
@@ -0,0 +1,10 @@
+##
+##  Matlab ode solver plugin
+##  This plugin uses the matlab python engine to approximate solutions to ODEs
+##
+
+import matlab.engine
+
+if __name__ == "__main__":
+    exit()
+
