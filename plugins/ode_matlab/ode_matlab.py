69a8280e086498c5d142d640f1e7b31d40516f51
Ezbob <andersbusch@gmail.com>
2016-09-19 11:01:04 +0200

    got wraps?

--- plugins/ode_matlab/ode_matlab.py
+++ plugins/ode_matlab/ode_matlab.py
@@ -3,24 +3,51 @@
 #  This plugin uses the matlab python engine to approximate solutions to ODEs
 ##
 import sys
+import matlab.engine
+
+
+class MatlabOde:
+
+    ode_solver = "ode45"
+    integration_range = (0, 0)
+    init_conditions = (0,)
+
+    def __init__(self, eq_system):
+        if isinstance(eq_system, str):
+            self.diff = eq_system
+        self.engine = matlab.engine.start_matlab()
+
+    def set_integration_range(self, range_tuple):
+        if isinstance(range_tuple, (tuple, list)):
+            self.integration_range = range_tuple
+        else:
+            self.engine.exit()
+            raise TypeError("Range not a tuple")
+
+    def set_initial_conditions(self, conditions):
+        if isinstance(conditions, (tuple, list)):
+            self.integration_range = conditions
+        else:
+            self.engine.exit()
+            raise TypeError("Range not a tuple")
+
+    def set_ode_solver(self, name):
+        if isinstance(name, str):
+            self.ode_solver = name
+
+    def solve(self):
+        self.engine.workspace['y0'] = matlab.double(self.init_conditions)
+        self.engine.workspace['tspan'] = matlab.double(self.integration_range)
+
+        tres, yres = self.engine.eval(self.ode_solver + "(" + self.diff + ", tspan, y0)", nargout=2)
+        self.engine.clear()
+        return tres, yres
+
+    def __del__(self):
+        self.engine.exit()
+
 
 if __name__ != "__main__":
-    import matlab.engine
-
-    engine = matlab.engine.start_matlab()
-
+    pass
 else:
     print("Plugin not meant as standalone application", file=sys.stderr)
-
-
-def stop_engine():
-    global engine
-    engine.quit()
-
-
-def create_matlab_func(ode_func):
-    pass
-
-
-def set_ode_solver(solver_name):
-    pass
