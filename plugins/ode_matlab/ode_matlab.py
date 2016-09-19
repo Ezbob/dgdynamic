70ad16bf7d2f0d62caa0ae50acbd029bcca58cd7
Ezbob <andersbusch@gmail.com>
2016-09-19 11:24:02 +0200

    Added enum to identify the different solvers

--- plugins/ode_matlab/ode_matlab.py
+++ plugins/ode_matlab/ode_matlab.py
@@ -4,17 +4,38 @@
 ##
 import sys
 import matlab.engine
+from enum import Enum
+
+
+class MatlabOdeSolvers(Enum):
+    ode45 = "45"
+    ode23 = "23"
+    ode113 = "113"
+    ode15s = "15s"
+    ode23s = "23s"
+    ode23t = "23t"
+    ode23tb = "23tb"
+    ode15i = "15i"
 
 
 class MatlabOde:
-
-    ode_solver = "ode45"
+    """
+    Wrapper for working with odes using the MATLAB python engine.
+    """
+    ode_solver = MatlabOdeSolvers.ode45
     integration_range = (0, 0)
     init_conditions = (0,)
 
-    def __init__(self, eq_system):
+    def __init__(self, eq_system, solver=MatlabOdeSolvers.ode45, integration_range=(0, 0), init_conditions=(0,)):
         if isinstance(eq_system, str):
             self.diff = eq_system
+        if isinstance(integration_range, (tuple, list)):
+            self.integration_range = integration_range
+        if isinstance(init_conditions, (tuple, list)):
+            self.init_conditions = init_conditions
+        if isinstance(solver, MatlabOdeSolvers):
+            self.ode_solver = solver
+
         self.engine = matlab.engine.start_matlab()
 
     def set_integration_range(self, range_tuple):
@@ -32,14 +53,15 @@ class MatlabOde:
             raise TypeError("Range not a tuple")
 
     def set_ode_solver(self, name):
-        if isinstance(name, str):
+        if isinstance(name, MatlabOdeSolvers):
             self.ode_solver = name
 
     def solve(self):
         self.engine.workspace['y0'] = matlab.double(self.init_conditions)
         self.engine.workspace['tspan'] = matlab.double(self.integration_range)
 
-        tres, yres = self.engine.eval(self.ode_solver + "(" + self.diff + ", tspan, y0)", nargout=2)
+        eval_str = "ode" + str(self.ode_solver.value) + "(" + self.diff + ", tspan, y0)"
+        tres, yres = self.engine.eval(eval_str, nargout=2)
         self.engine.clear()
         return tres, yres
 
@@ -47,7 +69,5 @@ class MatlabOde:
         self.engine.exit()
 
 
-if __name__ != "__main__":
-    pass
-else:
+if __name__ == "__main__":
     print("Plugin not meant as standalone application", file=sys.stderr)
