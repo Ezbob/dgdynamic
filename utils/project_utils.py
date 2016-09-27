f391d0db4b78367491225253021f6f0c94dd596d
Ezbob <andersbusch@gmail.com>
2016-09-27 12:03:01 +0200

    fixed the logger so it shows the module name combined the class name

--- utils/project_utils.py
+++ utils/project_utils.py
@@ -24,7 +24,7 @@ class LogMixin:
     """
     @property
     def logger(self):
-        name = ".".join([__name__, self.__class__.__name__])
+        name = ".".join([self.__class__.__module__, self.__class__.__name__])
         return logging.getLogger(name)
 
 
