755466e12ef3715cff7017bfe0009430ae779bab
Ezbob <andersbusch@gmail.com>
2016-09-28 09:55:06 +0200

    hello

--- plugins/ode_matlab/abstract_converter.py
+++ plugins/ode_matlab/abstract_converter.py
@@ -1,9 +1,12 @@
 from mod_interface.ode_generator import *
+from io import StringIO
 
 
 class MatlabSymbols(DefaultFunctionSymbols):
     function_start = "@(t,y) ["
+    equation_separator = ","
     function_end = "]"
+    pow = ".^"
 
 
 def get_malab_lambda(abstract_ode_system):
@@ -14,16 +17,20 @@ def get_malab_lambda(abstract_ode_system):
     # Parameter (also Symbol) -> parameter id
     parameter_map = {v: k for k, v in enumerate(abstract_ode_system.parameters)}
 
-    for vertex_id, expr in abstract_ode_system.generate_equations():
-        for item in sp.postorder_traversal(expr):
-            if len(item.args) > 0:
-                # print("arg {} func {}".format(item.args, item.func))
-                for arg in item.args:
-                    if arg.is_real:
-                        pass
-                        # print(float(arg))
-                    if arg.is_Symbol:
-                        try:
-                            print("I came from rev {} ".format(rev[arg]))
-                        except KeyError:
-                            print("I came from rev1 {} ".format(rev1[arg]))
+    with StringIO as strout:
+        strout.write(MatlabSymbols.function_start)
+        for vertex_id, expr in abstract_ode_system.generate_equations():
+            for item in sp.postorder_traversal(expr):
+                if len(item.args) > 0:
+                    # print("arg {} func {}".format(item.args, item.func))
+                    for arg in item.args:
+                        if arg.is_real:
+                            strout.write(float(arg))
+                        if arg.is_Symbol:
+                            try:
+                                strout.write("y({})".format(symbol_map[arg]))
+                            except KeyError:
+                                strout.write("k{}".format(parameter_map[arg]))
+            strout.write(MatlabSymbols.equation_separator)
+        strout.write(MatlabSymbols.function_end)
+        return strout.getvalue()
