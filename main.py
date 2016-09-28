9cbe52d666c6ce2b9153e89c1617b0be53f07b6c
Ezbob <andersbusch@gmail.com>
2016-09-28 14:58:35 +0200

    converter now converts, seeing the first results

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
+from src.mod_interface.ode_generator import AbstractOdeSystem
+from src.converters import matlab_converter
 from src.plugins.ode_scipy.scipy import ScipyOde
 from src.plugins.ode_matlab.matlab import MatlabOde # linking order is important so don't make this the first import
 from src.utils.project_utils import set_logging
 
 set_logging(new_session=True)
 
-matlab_ode = MatlabOde("@(t, y) y * 2 - 3", integration_range=(0, 5), initial_conditions={0: 4})
+aos = AbstractOdeSystem("""
+A + B -> C + C
+A + C -> D
+C -> E + F
+F + F -> B
+""")
 
-output = matlab_ode.solve()
+initial_conditions = {0: range(1, len(aos.symbols) + 1)} # 1, 3, 5, 7
+func = matlab_converter.get_malab_lambda(aos, list(range(1, len(aos.symbols) + 1)))
+matlab_ode = MatlabOde(func, initial_conditions=initial_conditions, integration_range=(0, 10))
+matlab_ode.solve().plot()
 
-output.save(name="firstordertest")
-output.plot()
-matlab_ode.set_ode_function("@(t,y) [ y(2); (1 - y(1) ^ 2) * y(2) - y(1) ]")\
-    .set_initial_conditions({0: [2, 3]}).set_integration_range((-10, 10))
-
-output = matlab_ode.solve()
-output.save(name="secondordertest")
-output.plot()
-
-scipy_ode = ScipyOde("lambda t, y: y * 2 - 3", integration_range=(0, 5), initial_condition={0: 4})
-
-output = scipy_ode.solve()
-output.save("firstordertest")
-print(output)
-output.plot()
-
-
-def f(t, y):
-    return [y[1], (1 - y[0] ** 2) * y[1] - y[0]]
-
-scipy_ode.set_ode_function(f).set_integration_range((-10, 10)).set_initial_conditions({0: [2, 3]})
-
-output = scipy_ode.solve()
-output.save("secondordertest")
-print(output)
-output.plot()
+# matlab_ode = MatlabOde("@(t, y) y * 2 - 3", integration_range=(0, 5), initial_conditions={0: 4})
+#
+# output = matlab_ode.solve()
+#
+# output.save(name="firstordertest")
+# output.plot()
+# matlab_ode.set_ode_function("@(t,y) [ y(2); (1 - y(1) ^ 2) * y(2) - y(1) ]")\
+#     .set_initial_conditions({0: [2, 3]}).set_integration_range((-10, 10))
+#
+# output = matlab_ode.solve()
+# output.save(name="secondordertest")
+# output.plot()
+#
+# scipy_ode = ScipyOde("lambda t, y: y * 2 - 3", integration_range=(0, 5), initial_condition={0: 4})
+#
+# output = scipy_ode.solve()
+# output.save("firstordertest")
+# print(output)
+# output.plot()
+#
+#
+# def f(t, y):
+#     return [y[1], (1 - y[0] ** 2) * y[1] - y[0]]
+#
+# scipy_ode.set_ode_function(f).set_integration_range((-10, 10)).set_initial_conditions({0: [2, 3]})
+#
+# output = scipy_ode.solve()
+# output.save("secondordertest")
+# print(output)
+# output.plot()
--- src/converters/matlab_converter.py
+++ src/converters/matlab_converter.py
@@ -3,9 +3,13 @@ from io import StringIO
 
 class MatlabSymbols:
     function_start = "@(t,y) ["
-    equation_separator = ","
+    equation_separator = ";"
     function_end = "]"
-    pow = ".^"
+    pow = "^"
+
+
+def _postprocessor(function_string):
+    return function_string.replace('**', MatlabSymbols.pow)
 
 
 def get_malab_lambda(abstract_ode_system, parameter_substitutions=None):
@@ -21,7 +25,7 @@ def get_malab_lambda(abstract_ode_system, parameter_substitutions=None):
     else:
         parameter_map = None
 
-    substitute_me = {value: "y({})".format(key) for key, value in abstract_ode_system.symbols.items()}
+    substitute_me = {value: "y({})".format(key + 1) for key, value in enumerate(abstract_ode_system.symbols.values())}
 
     with StringIO() as matlab_string:
 
@@ -31,15 +35,11 @@ def get_malab_lambda(abstract_ode_system, parameter_substitutions=None):
         if parameter_map is None:
             for vertex_id, equation in generated_functions:
                 matlab_string.write(str(equation.subs(substitute_me)))
-
-                if vertex_id < len(generated_functions) - 1:
-                    matlab_string.write("{} ".format(MatlabSymbols.equation_separator))
+                matlab_string.write("{} ".format(MatlabSymbols.equation_separator))
         else:
             for vertex_id, equation in generated_functions:
                 matlab_string.write(str(equation.subs(substitute_me).subs(parameter_map)))
-
-                if vertex_id < len(generated_functions) - 1:
-                    matlab_string.write("{} ".format(MatlabSymbols.equation_separator))
+                matlab_string.write("{} ".format(MatlabSymbols.equation_separator))
 
         matlab_string.write(MatlabSymbols.function_end)
-        return matlab_string.getvalue()
+        return _postprocessor(matlab_string.getvalue())
