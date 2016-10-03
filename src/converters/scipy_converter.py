570c94e0de204c061fb00fe008893eb3e68b93cb
Ezbob <andersbusch@gmail.com>
2016-10-03 17:11:56 +0200

    cleaned up the code a little

--- src/converters/scipy_converter.py
+++ src/converters/scipy_converter.py
@@ -3,14 +3,10 @@ from .converter import substitute, get_parameter_map
 from ..mod_interface.ode_generator import AbstractOdeSystem
 
 
-def _postprocessor(string_input: str):
-    return eval(string_input)
-
-
 def get_scipy_lambda(abstract_system: AbstractOdeSystem, parameter_substitutions=None):
 
     parameter_map = get_parameter_map(abstract_system, parameter_substitutions)
 
     substitute_me = {value: sp.Indexed('y', key) for key, value in enumerate(abstract_system.symbols.values())}
 
-    return substitute(abstract_system.generate_equations(), parameter_map, substitute_me, postprocessor=_postprocessor)
+    return substitute(abstract_system.generate_equations(), parameter_map, substitute_me)
--- src/plugins/ode_scipy/scipy.py
+++ src/plugins/ode_scipy/scipy.py
@@ -42,6 +42,7 @@ class ScipyOde(OdePlugin, LogMixin):
         if type(self._user_function) is str:
             self._user_function = eval(self._user_function)
 
+        self.logger.debug("Checking scipy parameters...")
         initial_t, initial_y = self.initial_conditions.popitem()
         sanity_check(self, initial_y)
 
@@ -49,53 +50,50 @@ class ScipyOde(OdePlugin, LogMixin):
         self.logger.debug("Initial conditions is {}, \
 range: {} and dt: {} ".format(self.initial_conditions, self.integration_range, self.delta_t))
 
+        y_solution = list()
+        t_solution = list()
+        solver = ode(self._user_function).set_integrator(self._solver_method.value.upper())
+        solver.set_initial_value(y=initial_y, t=initial_t)
+        solver.t = self.integration_range[0]
+
         def fixed_step_integration():
-            self.logger.debug("Setting scipy parameters...")
-
-            solver = ode(self._user_function).set_integrator(self._solver_method.value.upper())
-            solver.set_initial_value(initial_y, initial_t)
-            self.logger.debug("Set.")
-
-            ys = list()
-            ts = list()
-            solver.t = self.integration_range[0]
             try:
                 while solver.successful() and solver.t <= self.integration_range[1]:
-                    ts.append(solver.t)
-                    ys.append(solver.y)
+                    y_solution.append(solver.t)
+                    t_solution.append(solver.y)
                     solver.integrate(solver.t + self.delta_t)
-            except SystemError:
+            except SystemError as integration_error:
+                self.logger.exception("Integration process failed", integration_error)
                 return None
 
-            self.logger.debug("Solving finished")
-            if len(ys) > 0 and len(ts) > 0:
-                return OdeOutput(SupportedSolvers.Scipy, ys, ts, self._ignored)
+            if len(y_solution) > 0 and len(t_solution) > 0:
+                return OdeOutput(SupportedSolvers.Scipy, y_solution, t_solution, self._ignored)
             return None
 
         def variable_step_integration():
-
-            y_solution = []
-            t_solution = []
 
             def solution_getter(t, y):
                 y_solution.append(y.copy())
                 t_solution.append(t)
 
-            solver = ode(self._user_function).set_integrator(self._solver_method.value)
             solver.set_solout(solout=solution_getter)
-            solver.set_initial_value(y=initial_y, t=initial_t)
-            solver.t = self.integration_range[0]
 
-            while solver.successful() and solver.t < self.integration_range[1]:
-                solver.integrate(self.integration_range[1], step=True)
+            try:
+                while solver.successful() and solver.t < self.integration_range[1]:
+                    solver.integrate(self.integration_range[1], step=True)
+            except SystemError as integration_error:
+                self.logger.exception("Integration process failed", integration_error)
+                return None
 
             if len(y_solution) > 0 and len(t_solution) > 0:
                 return OdeOutput(SupportedSolvers.Scipy, y_solution, t_solution, self._ignored)
             return None
 
         if self._solver_method is ScipyOdeSolvers.DOP853 or self._solver_method is ScipyOdeSolvers.DOPRI5:
+            self.logger.debug("Solving finished using variable step integration")
             return variable_step_integration()
         else:
+            self.logger.debug("Solving finished using fixed step integration")
             return fixed_step_integration()
 
     def set_ode_solver(self, method: ScipyOdeSolvers):
