cbdeb14cc9c9e99025d9b0bd093b452ce15982cb
Ezbob <andersbusch@gmail.com>
2016-10-10 19:04:53 +0200

    damn.. I maybe this is a good result

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ root_symbol = 'A'
 species_limit = 60
 dimension_limit = species_limit // 2 + 1
 epsilon = numpy.nextafter(0, 1)
+theta = numpy.nextafter(1, 0)
 
-integration_range = (0, 50)
+integration_range = (0, 600)
 
+# this corresponds to setting the r_ij values
 disabled_reactions = [
     (1, 1)
 ]
+
+unchanging_species = (
+    'A1',
+)
 
 
 def get_symbols():
@@ -23,8 +29,8 @@ def get_symbols():
 def get_reactions():
     for i in range(1, dimension_limit):
         for j in range(1, dimension_limit):
-            it = (i == i_disabled and j == j_disabled for i_disabled, j_disabled in disabled_reactions)
-            if i <= j and not any(it):
+            in_disabled = (i == i_disabled and j == j_disabled for i_disabled, j_disabled in disabled_reactions)
+            if i <= j and not any(in_disabled):
                 yield "{0}{1} + {0}{2} <=> {0}{3}".format(root_symbol, i, j, i + j)
 
 print("A1 + A1 <=> A2" in tuple(get_reactions()))
@@ -33,10 +39,12 @@ reactions = "\n".join(get_reactions())
 
 initial_conditions = {}
 
-for symbol in get_symbols():
-    initial_conditions[symbol] = 0.0005
+a_1_concentration = theta
 
-initial_conditions['A1'] = 0.9
+for symbol in get_symbols():
+    initial_conditions[symbol] = 1.0 - a_1_concentration
+
+initial_conditions['A1'] = a_1_concentration
 
 parameters = {}
 
@@ -45,12 +53,12 @@ parameters = {}
 # k_s and k_d marks the rate of synthesis and decomposition
 
 for reaction in get_reactions():
-    parameters[reaction] = 0.5
+    parameters[reaction] = 0.6
 
 
 dg = mod.dgAbstract(reactions)
 
-ode = dgODESystem(dg)
+ode = dgODESystem(dg).unchanging_species(*unchanging_species)
 
 
 solver = ode.get_ode_plugin("scipy")
@@ -58,7 +66,9 @@ solver = ode.get_ode_plugin("scipy")
 solver.set_integration_range(integration_range)
 solver.set_parameters(parameters)
 solver.set_initial_conditions(initial_conditions)
-solver.set_ode_solver(ScipyOdeSolvers.DOPRI5)
+solver.set_ode_solver(ScipyOdeSolvers.LSODA)
+
+solver.delta_t = 0.1
 
 solver.solve().plot(figure_size=(60, 30))
 
