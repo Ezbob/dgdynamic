31e1e6521b7819ec6c238597c633d034aac0fd40
Ezbob <andersbusch@gmail.com>
2016-11-03 12:30:13 +0100

    cleaned up the mod_dynamics

--- dgDynamic/mod_dynamics.py
+++ dgDynamic/mod_dynamics.py
@@ -1,16 +1,6 @@
 from dgDynamic.simulators.ode_simulator import ODESystem
 from dgDynamic.simulators.stochastic_pi_simulator import StochasticPiSystem
 from typing import Union
-import enum
-
-
-class SupportedSolvers(enum.Enum):
-    """
-    Here we list the different solvers available in our system as key-value pairs.
-    The value is the internal system name for the solver and is used when saving the data files etc.
-    """
-    Scipy = "scipy"
-    Matlab = "matlab"
 
 
 def dgDynamicSim(graph, simulator_choice="ode") -> Union[ODESystem, StochasticPiSystem]:
