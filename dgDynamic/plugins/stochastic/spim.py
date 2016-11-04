5bc50b501ec56e6f8a06690015dffa0eefc7157a
Ezbob <andersbusch@gmail.com>
2016-11-04 20:22:41 +0100

    filled in some method that needs to be there

--- dgDynamic/plugins/stochastic/spim.py
+++ dgDynamic/plugins/stochastic/spim.py
@@ -7,7 +7,14 @@ class StochasticSpim:
         self._spim_path = config['Simulation']['SPIM_PATH']
 
         if self._spim_path is None or not self._spim_path:
-            self._spim_path = os.path.abspath()
+            self._spim_path = os.path.abspath()  # TODO INCLUDE SPIM.OCAML IF CONFIG IS BLANK
+        else:
+            self._spim_path = os.path.abspath(self._spim_path)
 
-        self._ocaml_path = config['Simulation']['OCAML_PATH']
+        self._ocamlrun_path = os.path.abspath(config['Simulation']['OCAML_RUN'])
 
+    def solve(self):
+        raise NotImplementedError
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
