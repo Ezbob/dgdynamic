a6d6e6a6c152664682b16f64da27f7ce3ceed5ea
Ezbob <andersbusch@gmail.com>
2016-11-08 10:05:35 +0100

    you can now set the title of the plot

--- dgDynamic/plugins/plugin_base.py
+++ dgDynamic/plugins/plugin_base.py
@@ -34,11 +34,12 @@ class SimulationOutput(LogMixin):
     def __str__(self):
         return "independent variable: {}\ndependent variable: {}".format(self.independent, self.dependent)
 
-    def plot(self, filename=None, labels=None, figure_size=None, axis_labels=None, axis_limits=None, should_wait=False,
-             timeout=10):
-        title = self.solver_used.name.title()
-        if self.solver_method_used is not None:
-            title += (" - " + self.solver_method_used.name)
+    def plot(self, filename=None, labels=None, figure_size=None, axis_labels=None, axis_limits=None, title=None,
+             should_wait=False, timeout=10):
+        if title is None:
+            title = self.solver_used.name.title()
+            if self.solver_method_used is not None:
+                title += (" - " + self.solver_method_used.name)
 
         # using queue here it's process-safe
         queue = mp.Queue()
