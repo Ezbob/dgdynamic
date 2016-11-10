ae8337d98d6990864aebbab0174dddecb2f78a5b
Ezbob <andersbusch@gmail.com>
2016-11-10 12:19:45 +0100

    config stuff

--- dgDynamic/config/settings.py
+++ dgDynamic/config/settings.py
@@ -1,6 +1,17 @@
 import configparser
 import os.path
 
-config = configparser.ConfigParser()
-config.read_file(open(os.path.join(os.path.dirname(__file__), 'default_config.ini')))
-config.read(os.path.join(os.path.abspath(os.path.curdir), 'config.ini'))
+default_config_file = 'default_config.ini'
+
+config_file_names = [
+    'config.ini',
+]
+
+if __name__ == "__main__":
+    from sys import stderr
+    print("Configuration loader not meant as standalone script", file=stderr)
+else:
+    config = configparser.ConfigParser()
+    config.read_file(open(os.path.join(os.path.dirname(__file__), default_config_file)))
+    config_file_loaded = config.read([os.path.join(os.path.abspath(os.path.curdir), name)
+                                      for name in config_file_names])
