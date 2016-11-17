b4cd1109901d933a221283716b28a6493a3d2128
Ezbob <andersbusch@gmail.com>
2016-11-17 10:50:23 +0100

    added comments and typecheck for parameter values

--- dgDynamic/converters/convert_base.py
+++ dgDynamic/converters/convert_base.py
@@ -20,31 +20,36 @@ def get_edge_rate_dict(reaction_parser_function, user_parameters, internal_param
         raise InitialValueError("Reaction parser function is not a callable")
 
     def add_to_result(key, value):
-        if internal_parameters_map is not None:
-            result[internal_parameters_map[key]] = value
+        if isinstance(value, (int, float)):
+            if internal_parameters_map is not None:
+                result[internal_parameters_map[key]] = value
+            else:
+                result[key] = value
         else:
-            result[key] = value
+            raise TypeError("Type error for key {}; expected float or int".format(key))
 
     if isinstance(user_parameters, dict):
-        for reaction_string, rate in user_parameters.items():
-            parsed_edges = reaction_parser_function(reaction_string)
+        for key, rate in user_parameters.items():
+            parsed_edges = reaction_parser_function(key)
             if isinstance(rate, (int, float)):
+                # rhs is simply a number
                 if isinstance(parsed_edges, tuple):
                     for parsed_edge in parsed_edges:
                         add_to_result(parsed_edge.id, rate)
                 else:
                     add_to_result(parsed_edges.id, rate)
             elif isinstance(rate, (tuple, list, set)):
+                # rhs is a iterable
                 if isinstance(parsed_edges, tuple):
                     if len(rate) < len(parsed_edges):
-                        raise InitialValueError("Not enough initial conditions given for reaction: {}"
-                                                .format(reaction_string))
+                        raise InitialValueError("Not enough initial conditions given for reaction: {}".format(key))
                     for parsed_edge, rate_item in zip(parsed_edges, rate):
                         add_to_result(parsed_edge, rate_item)
                 else:
                     raise InitialValueError("Multivalued initial condition given for reaction: {}"
-                                            .format(reaction_string))
+                                            .format(key))
             elif isinstance(rate, dict):
+                # right hand side is a dictionary of the form { '->': ..., '<-': ... } or { '<=>': ... }
                 if isinstance(parsed_edges, tuple):
                     if '<=>' in rate:
                         for parsed_edge in parsed_edges:
@@ -54,14 +59,15 @@ def get_edge_rate_dict(reaction_parser_function, user_parameters, internal_param
                         add_to_result(parsed_edges[1].id, rate['<-'])
                     else:
                         raise InitialValueError("Not enough initial conditions given for reaction: {}"
-                                                .format(reaction_string))
+                                                .format(key))
                 else:
                     raise InitialValueError("Multivalued initial condition given for reaction: {}"
-                                            .format(reaction_string))
+                                            .format(key))
             else:
                 raise InitialValueError("Unsupported type {} of initial condition for reaction: {} "
-                                        .format(type(rate), reaction_string))
+                                        .format(type(rate), key))
     elif isinstance(user_parameters, (tuple, list, set)):
+        # user parameters is just a iterable
         for index_rate, rate in enumerate(user_parameters):
             add_to_result(index_rate, rate)
 
