d965641329d80f3fde82ecd43e00e26377720c9c
Ezbob <andersbusch@gmail.com>
2016-11-17 21:29:34 +0100

    added formose to the script folder

---
+++ scripts/formose/grammar.py
@@ -0,0 +1,6 @@
+formaldehyde = smiles("C=O", "Formaldehyde")
+glycolaldehyde = smiles("O=CCO", "Glycolaldehyde")
+ketoEnol_F = ruleGML("keto_enol_forward.gml")
+ketoEnol_B = ruleGML("keto_enol_backward.gml")
+aldolAdd_F = ruleGML("aldol_addition_forward.gml")
+aldolAdd_B = ruleGML("aldol_addition_backward.gml")
