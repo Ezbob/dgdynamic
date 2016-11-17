95de5e72e44161ba9e63c50d90a99c0088c52500
Ezbob <andersbusch@gmail.com>
2016-11-17 22:13:54 +0100

    blood for the blood god!

--- scripts/formose/formose.py
+++ scripts/formose/formose.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 
+# Setting the Python Path so we can use dgDynamic
 dynamic_module_path = os.path.abspath("../../")
 sys.path.append(dynamic_module_path)
 
@@ -40,6 +41,7 @@ solution = list(flow.solutions)[0]
 flow_graph = HyperGraph.from_flow_solution(solution)
 
 ode = dgDynamicSim(flow_graph)
+stochastic = dgDynamicSim(flow_graph, simulator_choice="stochastic")
 
 parameters = {AbstractReaction(dg, edge): 0.5 for edge in ode.abstract_edges}
 
@@ -52,26 +54,22 @@ sim_range = (0, 15)
 
 scipy = ode("scipy")
 
-scipy(sim_range, initial_conditions, parameters).plot(figure_size=(40, 20))
+scipy(sim_range, initial_conditions, parameters).plot(filename="scipy_plot.png",
+    figure_size=(40, 20), title="Formose cycle solution simulation")
+
+#sim_range = (100, 100)
+
+# Not working due to how the naming convention of generic compounds are in MØD (P_{0,0} and so on).
+# Pretty sure it is the either the curly brackets or the in-name underscore
+#for i in range(3):
+#    stochastic("spim")(sim_range, initial_conditions, parameters, timeout=120).plot(
+#        filename="spim_plot{}.png".format(i), figure_size=(40, 20), title="{}. Formose cycle solution simulation")
 
 show_simulation_plots()
 
 #def get_flow_solution_graph(solution):
 #    """Get a tuple of hyper vertices and hyper edges from a flow solution"""
 #    orign_graph = solution.dgFlow.dg
-#    return tuple( v for v in orign_graph.vertices if solution.eval(vertex(v.graph)) != 0.0), tuple( e for e in orign_graph.edges if solution.eval(edge(e)) != 0.0)
-
-
-#parameters = {
-#    HyperEdge(
-#        ('A', 'B',), 'C'
-#    ),
-#    HyperEdge(
-#        ('B', 'D'), 'A'
-#    )
-#}
-
-# How to find the vertices and edges of flow solutions
-# sols = [(tuple(v for v in dg.vertices if sol.eval(vertex(v.graph)) != 0.0), tuple( e for e in dg.edges if
-# sol.eval(edge(e)) != 0.0)) for sol in flowAutocata.solutions]
+#    return tuple( v for v in orign_graph.vertices if solution.eval(vertex(v.graph)) != 0.0),
+# tuple( e for e in orign_graph.edges if solution.eval(edge(e)) != 0.0)
 
