e8f3d5464318ad06e02c14cc959db91cf6b1af5f
Ezbob <andersbusch@gmail.com>
2016-12-07 17:03:10 +0100

    can't seem to get the amplitude??

--- dgDynamic/analytics_module.py
+++ dgDynamic/analytics_module.py
@@ -4,45 +4,38 @@ from dgDynamic.utils.project_utils import pop_or_default
 
 
 class DynamicAnalytics:
-    def __init__(self, simulation_output: SimulationOutput, sample_rate=None):
+    def __init__(self, simulation_output: SimulationOutput, delta_t=None, sample_rate=None, windowing_function=None):
         self.output = simulation_output
-        self.windowing_function = np.hanning
+        self.windowing_function = windowing_function
         self.sample_size = len(self.output.dependent)
         # default: assuming uniform spacing between time samples
         self.sample_rate = self.sample_size / self.output.simulation_duration \
             if sample_rate is None else sample_rate
+        self.sample_spacing = 1 / self.sample_rate if delta_t is None else delta_t
 
-    def generate_fourier_transformations(self, bins=None, normalize_with=None, *args, **kwargs):
+    def generate_fourier_transformations(self, bins=None, *args, **kwargs):
         for dependent_index in range(self.output.dependent_dimension):
             numpy_array = np.fromiter((self.output.dependent[index][dependent_index]
                                        for index in range(len(self.output))), dtype=float)
             if self.windowing_function is not None:
                 window_convolution = numpy_array * self.windowing_function(self.sample_size)
-                if normalize_with is None:
-                    yield np.fft.rfft(window_convolution, n=bins, *args, **kwargs)
-                else:
-                    yield np.fft.rfft(window_convolution, n=bins, *args, **kwargs) / normalize_with
+                yield np.fft.rfft(window_convolution, n=bins, *args, **kwargs) * self.sample_spacing
             else:
-                if normalize_with is None:
-                    yield np.fft.rfft(numpy_array, n=bins, *args, **kwargs)
-                else:
-                    yield np.fft.rfft(numpy_array, n=bins, *args, **kwargs) / normalize_with
+                yield np.fft.rfft(numpy_array, n=bins, *args, **kwargs) * self.sample_spacing
 
     def generate_amplitude_spectrum(self, with_freqs=False, *args, **kwargs):
-        sample_spacing = pop_or_default(kwargs, 'd', default=1.0)
         for fourier_trans in self.generate_fourier_transformations(*args, **kwargs):
             amplitudes = np.abs(fourier_trans) / self.sample_size
             if with_freqs:
-                yield amplitudes, np.fft.rfftfreq(self.sample_size, d=sample_spacing)
+                yield amplitudes, np.fft.rfftfreq(self.sample_size, d=self.sample_spacing)
             else:
                 yield amplitudes
 
     def generate_power_spectrum(self, with_freqs=False, *args, **kwargs):
-        sample_spacing = pop_or_default(kwargs, 'd', default=1.0)
         for trans in self.generate_fourier_transformations(*args, **kwargs):
             power_spectrum = (np.abs(trans) / self.sample_size) ** 2
             if with_freqs:
-                yield power_spectrum, np.fft.rfftfreq(self.sample_size, d=sample_spacing)
+                yield power_spectrum, np.fft.rfftfreq(self.sample_size, d=self.sample_spacing)
             else:
                 yield power_spectrum
 
