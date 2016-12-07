pretty sure I fixed the scaling finally

dgDynamic/analytics_module.py
@@ -13,19 +13,24 @@ class DynamicAnalytics:
             if sample_rate is None else sample_rate
         self.sample_spacing = 1 / self.sample_rate if delta_t is None else delta_t
 
+    def _scale_fourier(self, fourier):
+        # http://stackoverflow.com/questions/15147287/numpy-wrong-amplitude-of-fftd-array
+        return np.fromiter((f if index == 0 else f * 2 for index, f in enumerate(fourier)), dtype=float) \
+               / self.sample_size
+
     def generate_fourier_transformations(self, bins=None, *args, **kwargs):
         for dependent_index in range(self.output.dependent_dimension):
             numpy_array = np.fromiter((self.output.dependent[index][dependent_index]
                                        for index in range(len(self.output))), dtype=float)
             if self.windowing_function is not None:
                 window_convolution = numpy_array * self.windowing_function(self.sample_size)
-                yield np.fft.rfft(window_convolution, n=bins, *args, **kwargs) * self.sample_spacing
+                yield np.fft.rfft(window_convolution, n=bins, *args, **kwargs)
             else:
-                yield np.fft.rfft(numpy_array, n=bins, *args, **kwargs) * self.sample_spacing
+                yield np.fft.rfft(numpy_array, n=bins, *args, **kwargs)
 
     def generate_amplitude_spectrum(self, with_freqs=False, *args, **kwargs):
         for fourier_trans in self.generate_fourier_transformations(*args, **kwargs):
-            amplitudes = np.abs(fourier_trans) / self.sample_size
+            amplitudes = self._scale_fourier(np.absolute(fourier_trans))
             if with_freqs:
                 yield amplitudes, np.fft.rfftfreq(self.sample_size, d=self.sample_spacing)
             else:
@@ -33,7 +38,7 @@ class DynamicAnalytics:
 
     def generate_power_spectrum(self, with_freqs=False, *args, **kwargs):
         for trans in self.generate_fourier_transformations(*args, **kwargs):
-            power_spectrum = (np.abs(trans) / self.sample_size) ** 2
+            power_spectrum = self._scale_fourier(np.absolute(self._scale_fourier(trans)) ** 2)
             if with_freqs:
                 yield power_spectrum, np.fft.rfftfreq(self.sample_size, d=self.sample_spacing)
             else: