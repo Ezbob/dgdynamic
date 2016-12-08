9aaf5d7e6a0a2bfa9582c575f8e46b74629f704a
Ezbob <andersbusch@gmail.com>
2016-12-08 20:10:55 +0100

    class dgDynamicSim

--- dgDynamic/mod_dynamics.py
+++ dgDynamic/mod_dynamics.py
@@ -7,21 +7,22 @@ import enum
 import mod
 
 
-def dgDynamicSim(derivation_graph: mod.DG, simulator_choice: tp.Union[str, enum.Enum]="ode", unchanging_species:
-                 tp.Union[str, tuple]=()) -> tp.Optional[tp.Union[ODESystem, StochasticPiSystem]]:
-    deviation_graph = derivation_graph if isinstance(derivation_graph, HyperGraph) else HyperGraph(derivation_graph)
-    if isinstance(simulator_choice, str):
-        if simulator_choice.strip().lower() == "ode":
-            return ODESystem(graph=deviation_graph).unchanging_species(*unchanging_species)
-        elif simulator_choice.strip().lower() == "stochastic":
-            return StochasticPiSystem(graph=deviation_graph).unchanging_species(*unchanging_species)
-    elif isinstance(simulator_choice, enum.Enum):
-        if simulator_choice.name.lower() == "ode":
-            return ODESystem(graph=deviation_graph).unchanging_species(*unchanging_species)
-        elif simulator_choice.name.lower() == "stochastic_pi":
-            return StochasticPiSystem(graph=deviation_graph).unchanging_species(*unchanging_species)
-    else:
-        return None
+class dgDynamicSim:
+    def __new__(cls, derivation_graph: mod.DG, simulator_choice: tp.Union[str, enum.Enum]="ode", unchanging_species:
+                tp.Union[str, tuple]=()) -> tp.Optional[tp.Union[ODESystem, StochasticPiSystem]]:
+        deviation_graph = derivation_graph if isinstance(derivation_graph, HyperGraph) else HyperGraph(derivation_graph)
+        if isinstance(simulator_choice, str):
+            if simulator_choice.strip().lower() == "ode":
+                return ODESystem(graph=deviation_graph).unchanging_species(*unchanging_species)
+            elif simulator_choice.strip().lower() == "stochastic":
+                return StochasticPiSystem(graph=deviation_graph).unchanging_species(*unchanging_species)
+        elif isinstance(simulator_choice, enum.Enum):
+            if simulator_choice.name.lower() == "ode":
+                return ODESystem(graph=deviation_graph).unchanging_species(*unchanging_species)
+            elif simulator_choice.name.lower() == "stochastic_pi":
+                return StochasticPiSystem(graph=deviation_graph).unchanging_species(*unchanging_species)
+        else:
+            return None
 
 
 def show_plots(*args, **kwargs):
