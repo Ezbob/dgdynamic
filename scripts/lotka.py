d7290dc0a3d6540846259d6fc4aafa538b1bec56
Ezbob <andersbusch@gmail.com>
2016-12-13 13:46:37 +0100

    lotka has drain parameters

--- scripts/lotka.py
+++ scripts/lotka.py
@@ -9,9 +9,9 @@ from dgDynamic.choices import SupportedOdePlugins
 
 rabbit_multiples = "R -> 2 R"
 foxes_hunts = "R + F -> F + F"
-foxes_dies = "F -> D"
+# foxes_dies = "F -> D" # Now modeled as a drain parameter
 
-dg = HyperGraph.from_abstract(rabbit_multiples, foxes_hunts, foxes_dies)
+dg = HyperGraph.from_abstract(rabbit_multiples, foxes_hunts,)
 
 initial_conditions = {
     'F': 250,
@@ -21,26 +21,31 @@ initial_conditions = {
 parameters = {
     foxes_hunts: 0.005,
     rabbit_multiples: 0.7,
-    foxes_dies: 0.5,
+}
+
+drain_parameters = {
+    'F': {'in': 0, 'out': 0.5}
 }
 
 integration_range = (0, 100)
 
-ode = dgDynamicSim(dg, simulator_choice='ode', unchanging_species='D')
-stochastic = dgDynamicSim(dg, simulator_choice='stochastic', unchanging_species='D')
+ode = dgDynamicSim(dg, simulator_choice='ode')
+stochastic = dgDynamicSim(dg, simulator_choice='stochastic')
 
 # Name of the data set
 name = "foxesRabbits"
 figure_size = (40, 20)
 
 for ode_plugin_name in SupportedOdePlugins:
-    output = ode(ode_plugin_name)(integration_range, initial_conditions, parameters).plot(figure_size=figure_size)
+    output = ode(ode_plugin_name)(integration_range, initial_conditions, parameters, drain_parameters)\
+        .plot(figure_size=figure_size)
 
 spim_simulation_range = (100, 1000)
 
 with stochastic('spim') as spim:
+    spim.timeout = 60
     for i in range(8):
         spim(simulation_range=spim_simulation_range, initial_conditions=initial_conditions,
-             rate_parameters=parameters, timeout=60).plot(figure_size=figure_size)
+             rate_parameters=parameters, drain_parameters=drain_parameters,).plot(figure_size=figure_size)
 
 show_plots()
