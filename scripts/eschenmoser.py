344e974a005ed4f20889790f0f4751ab769810eb
Ezbob <andersbusch@gmail.com>
2017-02-10 14:16:18 +0100

    added cycles to echenmoser??

--- scripts/eschenmoser.py
+++ scripts/eschenmoser.py
@@ -1,40 +1,78 @@
 from dgDynamic import dgDynamicSim, HyperGraph, show_plots
 import random
 import numpy as np
+import enum
 
 rate_std_deviation = np.nextafter(1, 0)
 rate_mean = 0
 
-reacts = [
-    "4 HCN -> 2 N2H",
-    #"HCN + HCN -> HCN'",
-    "2 N2H -> 2 NOHNH2"
+
+class ImportantSpecies(enum.Enum):
+    HCN = "HCN"
+    Glyoxylate = "Glyoxylate"
+    Oxaloglycolate = "Oxaloglycolate"
+    Oxoaspartate = "Oxoaspartate"
+
+
+reactions = [
+    # Starting with the first autocatalytic cycle (on the green side)
+    "2 {} -> C1S1".format(ImportantSpecies.HCN.name),
+    "C1S1 -> C1S2",
+    "C1S2 + {} -> C1S3".format(ImportantSpecies.Glyoxylate.name),
+    "C1S3 -> C1S4",
+    "C1S4 -> C1S5",
+    "C1S5 -> C1S6 + {}".format(ImportantSpecies.Glyoxylate.name),
+    "C1S6 -> {}".format(ImportantSpecies.Glyoxylate.name),
+    # Next autocatalytic cycle (on the orange side)
+    "C2S10 + {} -> C2S1".format(ImportantSpecies.Glyoxylate.name),
+    "C2S1 -> C2S2",
+    "C2S2 -> C2S3",
+    "C2S3 -> {} + {}".format(ImportantSpecies.Oxaloglycolate.name, ImportantSpecies.Oxoaspartate.name),
+    "{} <=> C2S4".format(ImportantSpecies.Oxaloglycolate.name),
+    "C2S4 <=> C2S5",
+    "C2S5 <=> C2S6",
+    "{} -> C2S6".format(ImportantSpecies.Oxoaspartate.name),
+    "{} + C2S6 -> C2S7".format(ImportantSpecies.Glyoxylate.name),
+    "C2S7 -> C2S8",
+    "C2S8 -> C2S9",
+    "C2S9 -> C2S10"
 ]
 
 
-def random_rates(n, mean=0, std_deviation=1):
-    for i in range(n):
-        rand = random.gauss(mean, std_deviation)
-        if rand < 0.0:
-            rand = abs(rand)
-        yield rand
+def generate_rates(number_of_reactions, decomposed_rates=()):
+    results = [0.0] * number_of_reactions
+    decomposed_args = []
 
+    for decompose_set in decomposed_rates:
+        # decomposition of "chain reactions" into elementary reactions yields probability k / p where p
+        # are the number reactions that the decomposition yields
+        rand = random.random() / len(decompose_set)
+        for arg in decompose_set:
+            results[arg] = rand
+            decomposed_args.append(arg)
 
-r = tuple(random_rates(len(reacts), rate_mean, rate_std_deviation))
+    for i in range(number_of_reactions):
+        if i not in decomposed_args:
+            results[i] = random.random()
+    return results
 
-parameters = {key: val for key, val in zip(reacts, r)}
+r = generate_rates(len(reactions))
+
+print(r)
+parameters = {key: val for key, val in zip(reactions, r)}
 
 initial_conditions = {
-    'HCN': 10,
-    'N2H': 4,
-    'NOHNH2': 2
+    ImportantSpecies.HCN.name: 10,
+    ImportantSpecies.Glyoxylate.name: 2
 }
 
 print("Parameters are: ")
 for react, param in parameters.items():
     print("{} : {}".format(react, param))
 
-dg = HyperGraph.from_abstract(*reacts)
+dg = HyperGraph.from_abstract(*reactions)
+
+dg.print()
 
 stochastic = dgDynamicSim(dg, "stochastic")
 
@@ -44,6 +82,5 @@ with stochastic('stochkit2') as stochkit2:
     out = stochkit2.simulate(sim_range, initial_conditions, parameters)
 
     out.plot()
-
 
 show_plots()
