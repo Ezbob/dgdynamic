dd25812b7c93c7e5c554efc5b5157bf7552fc4b2
Ezbob <andersbusch@gmail.com>
2017-03-02 18:04:41 +0100

    trying to get init to work

--- shipping/init.sh
+++ shipping/init.sh
@@ -12,24 +12,24 @@ echo "     /____/                 "
 
 echo "By Anders Busch, 2017. Check the LICENSE file for licensing information."
 
-echo "Setting up virtual environment..."
+echo "Setting up virtual environment in "$CUR_DIR"..."
 
 N_THREADS=4
 LOG=$CUR_DIR"/dump.log"
 ERR=$CUR_DIR"/err.log"
 
-mkdir src >> $LOG 2>> $ERR
-mkdir python353 >> $LOG 2>> $ERR
-
 SRC_DIR=$CUR_DIR/src
 PY_DIR=$CUR_DIR/python353
 
+mkdir -p $SRC_DIR >> $LOG 2>> $ERR
+mkdir -p $PY_DIR >> $LOG 2>> $ERR
+
 echo "Extracting Python 3.5.3..."
-tar --extract -f Python-3.5.3.tgz -C $SRC_DIR >> $LOG 2>> $ERR
+tar --extract -f Python-3.5.3.tgz >> $LOG 2>> $ERR
 echo "done."
 
 echo "Compiling Python 3.5.3 from source..."
-cd $SRC_DIR/Python-3.5.3/
+cd $CUR_DIR/Python-3.5.3/
 ./configure --prefix=$PY_DIR >> $LOG 2>> $ERR
 make -j$N_THREADS >> $LOG 2>> $ERR
 make install >> $LOG 2>> $ERR
