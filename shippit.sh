efa607d1b2e2b2f105ebeca1c0e9bbffc62d43c2
Ezbob <andersbusch@gmail.com>
2017-03-02 17:18:19 +0100

    One can now use makeself to patch it all together

--- shippit.sh
+++ shippit.sh
@@ -2,11 +2,14 @@
 
 CUR_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 BUILD_DIR=$CUR_DIR/build
+COMMENT="Deviation Graph Dynamic Simulation Liberary version 0.9"
 
 # copy stuff over from the git index
 git checkout-index --all -f --prefix=$BUILD_DIR/
 cd $BUILD_DIR
-rm shippit.sh
+rm -rf shippit.sh tests/
 cd $CUR_DIR
 
+makeself --notemp --current $CUR_DIR/dgDynamic dgdsl.run $COMMENT $CUR_DIR/build/shipping/init.sh
 
+rm -rf $BUILD_DIR
