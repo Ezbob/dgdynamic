fc58f2c6bae546e9a03655239b88b26205ab86ae
Ezbob <andersbusch@gmail.com>
2017-03-19 18:13:07 +0100

    fixed the compile script

--- compile_stochkit2.sh
+++ compile_stochkit2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./dgDynamic/plugins/stochastic/stochkit2/StochKit/install.sh
+exec $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/dgDynamic/plugins/stochastic/stochkit2/StochKit/install.sh
