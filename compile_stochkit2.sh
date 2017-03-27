b483d99951bdc2cf6e5492c32ee71a129adfdf6b
Ezbob <andersbusch@gmail.com>
2017-03-27 21:46:39 +0200

    compile script now also extracts

--- compile_stochkit2.sh
+++ compile_stochkit2.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-exec $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/dgDynamic/plugins/stochastic/stochkit2/StochKit/install.sh
+CUR_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+cd $CUR_DIR/dgDynamic/plugins/stochastic/stochkit2/ 
+
+echo "Extracting StochKit2"
+tar --extract --file stochkit.tar.gz
+echo "done."
+echo "Running install script..."
+cd StochKit/
+. ./install.sh
+echo "done."
+echo "all done."
+
+cd $CUR_DIR
