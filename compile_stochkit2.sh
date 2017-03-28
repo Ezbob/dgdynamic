f3c061a41357c3b55dc905dbd4725625700595a3
Ezbob <andersbusch@gmail.com>
2017-03-28 19:40:12 +0200

    added stochkit2 compilation to setup

--- compile_stochkit2.sh
+++ compile_stochkit2.sh
@@ -5,7 +5,7 @@ CUR_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 cd $CUR_DIR/dgDynamic/plugins/stochastic/stochkit2/ 
 
 echo "Extracting StochKit2"
-tar --extract --file stochkit.tar.gz
+tar --extract --verbose --file stochkit.tar.gz
 echo "done."
 echo "Running install script..."
 cd StochKit/
--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup
+from setuptools.command.install import install
+import os
+import importlib
+import sys
+import atexit
 
 if __name__ == '__main__':
+
+    my_name = 'dgDynamic'
 
     package_dirs = [
         'dgDynamic',
@@ -30,15 +37,36 @@ if __name__ == '__main__':
         for p_name in package_dirs
     }
 
+    class CustomInstall(install):
+        def run(self):
+            def _post_install():
+                def find_module_path():
+                    for p in sys.path:
+                        if os.path.isdir(p):
+                            for f in os.listdir(p):
+                                if f == my_name:
+                                    return os.path.join(p, f)
+                install_path = find_module_path()
+                os.system("tar --extract --file " +
+                          os.path.join(install_path, "plugins/stochastic/stochkit2/stochkit.tar.gz"))
+                os.system("cd " + os.path.join(install_path, "plugins/stochastic/stochkit2/StochKit/") +
+                          "&& ./install.sh")
+
+            atexit.register(_post_install)
+
+            install.run(self)
+
     setup(
+        cmdclass={'install': CustomInstall},
         name='dgdynamic',
-        version='0.5',
+        version='0.5.2',
         description='Dynamic simulation library for the MØD graph transformation framework',
         url='https://bitbucket.org/Ezben/dgdynamic',
         author='Anders Busch',
         author_email='andersbusch@gmail.com',
         license='MIT',
         package_dir=p_dict_dir,
+        include_package_data=True,
         package_data={'': extras},
         packages=list(p_dict_dir.keys()),
         install_requires=[
