3221c62c6c257e9b73cea0732a635c4af63f4970
Ezbob <andersbusch@gmail.com>
2017-03-28 11:34:34 +0200

    fixed package info in setup file

--- setup.py
+++ setup.py
@@ -1,21 +1,44 @@
-
 from setuptools import setup
 
-setup(
-    name='dgdynamic',
-    version='0.5',
-    description='Dynamic simulation library for the MØD graph transformation framework',
-    url='https://bitbucket.org/Ezben/dgdynamic',
-    author='Anders Busch',
-    author_email='andersbusch@gmail.com',
-    license='MIT',
-    packages=['dgDynamic'],
-    install_requires=[
-        'scipy>=0.18.1',
-        'numpy>=1.11.2',
-        'sympy>=1.0',
-        'matplotlib>=1.5.2'
-    ],
-    zip_safe=False
-)
+if __name__ == '__main__':
+
+    package_dirs = [
+        'dgDynamic',
+        'dgDynamic/intermediate',
+        'dgDynamic/base_converters',
+        'dgDynamic/base_converters/ode',
+        'dgDynamic/utils',
+        'dgDynamic/config',
+        'dgDynamic/simulators',
+        'dgDynamic/plugins',
+        'dgDynamic/plugins/stochastic',
+        'dgDynamic/plugins/stochastic/stochkit2',
+        'dgDynamic/plugins/stochastic/spim',
+        'dgDynamic/plugins/ode',
+        'dgDynamic/plugins/ode/matlab',
+        'dgDynamic/plugins/ode/scipy'
+    ]
+    p_dict_dir = {
+        ".".join(p_name.split('/')): p_name
+        for p_name in package_dirs
+    }
+
+    setup(
+        name='dgdynamic',
+        version='0.5',
+        description='Dynamic simulation library for the MØD graph transformation framework',
+        url='https://bitbucket.org/Ezben/dgdynamic',
+        author='Anders Busch',
+        author_email='andersbusch@gmail.com',
+        license='MIT',
+        package_dir=p_dict_dir,
+        packages=list(p_dict_dir.keys()),
+        install_requires=[
+            'scipy>=0.18.1',
+            'numpy>=1.11.2',
+            'sympy>=1.0',
+            'matplotlib>=1.5.2'
+        ],
+        zip_safe=False
+    )
 
