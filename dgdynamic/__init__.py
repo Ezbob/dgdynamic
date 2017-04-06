9cf0e1d0dcd04d6c35c86764af5c39bcbbb641b5
Ezbob <andersbusch@gmail.com>
2017-04-06 21:08:15 +0200

    added a little description in the __init__ file

--- dgdynamic/__init__.py
+++ dgdynamic/__init__.py
@@ -1,4 +1,10 @@
+"""
+dgdynamic
+    A python 3 package for dynamic simulation on deviation graph created by the MØD framework.
+    Written by Anders Busch (2017).
+"""
 from .mod_dynamics import dgDynamicSim, show_plots
 from .structures import HyperGraph
-from .analytics import DynamicAnalysisDevice
-__all__ = []
+from .utils.exceptions import SimulationError, ModelError
+
+__all__ = ["dgDynamicSim", "show_plots", "HyperGraph"]
